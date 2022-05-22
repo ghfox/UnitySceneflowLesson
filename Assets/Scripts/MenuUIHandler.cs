ac1a58870ad525ab017684d5237d7d0a612c22d2
hwaycock <hwest.aycock@gmail.com>
2022-05-22 18:50:42 -0500

    basic save file

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -29,6 +29,18 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void Exit() 
@@ -38,5 +50,6 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit();
 #endif
+        MainManager.Instance.SaveColor();
     }
 }
